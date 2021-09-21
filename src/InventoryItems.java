bd97510fd4f0d2e4d407c8d6a7ba597ea94a78b2
h1carroll <90262864+h1carroll@users.noreply.github.com>
2021-09-21 09:30:39 -0400

    commit

--- src/InventoryItems.java
+++ src/InventoryItems.java
@@ -9,17 +9,17 @@ import java.util.ArrayList;
 public class InventoryItems {
     public static void main(String[] args) throws IOException{
         var fileName = "itemList";
-        var allLines = Files.readAllLines(filePath);
         var filePath  = Paths.get(fileName);
         var Product = new ArrayList<String>();
         var Price = new ArrayList<Float>();
         var Inventory = new ArrayList<Integer>();
+        var allLines = Files.readAllLines(filePath);
         for (var line: allLines){
             var splitLine = line.split(",");
             Product.add(splitLine[0]);
             Inventory.add(Integer.parseInt(splitLine[1]));
             Price.add(Float.parseFloat(splitLine[2]));
-
-
+            for(int i = 0; i<InventoryItems.size(); i++){
+                System.out.println("The item you are looking for is "+ Price.get(i) + " and there are "+ Inventory.get(i) + "in stock. ");
 
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,30 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InventoryItems{
-    public static void main(String[] args){
+    public static void main(String[] args)throws IOException {
+        var fileName = "ItemLlist";
+        String all_items = Files.readString(Paths.get(fileName));
+        String[] item_list = all_items.split("\n");
+        for (String name: item_list){
+            System.out.println(name);
+            public static void main(String[] args){
+                var Items = new ArrayList<String>();
+                var reader = new Scanner(System.in);
+                var userResponse = "";
+                while(!userResponse.equals("done")){
+                    System.out.print("Would you like to continue? Enter item name or enter 'done' to end");
+                    userResponse = reader.nextLine();
+                    if (!userResponse.equals("done")){
+                        Items.add(userResponse);
+                    }
+                }
 
-
-        var Items = new ArrayList<String>();
-        var reader = new Scanner(System.in);
-        var userResponse = "";
-        while(!userResponse.equals("done")){
-            System.out.print("Would you like to continue? Enter item name or enter 'done' to end");
-            userResponse = reader.nextLine();
-            if (!userResponse.equals("done")){
-                Items.add(userResponse);
+                }
             }
         }
-        System.out.println("The students in your class are: ");
-        for (var Name: studentNames){
-            System.out.println(Name);
-        }
-    }
-}
